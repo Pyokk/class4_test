5139a1ca514c3e749e79201d63036a6e2a1fbd67
owner <owner@owner-PC>
2017-12-13 14:31:27 +0800

    선빵

--- test_git/src/kr/co/sist/test/KyungPyo.java
+++ test_git/src/kr/co/sist/test/KyungPyo.java
@@ -4,7 +4,7 @@ public class KyungPyo {
 
 	public static void main(String[] args) {
 		System.out.println("kyungpyo");
-		System.out.println("zzzzzzzz");
+		System.out.println("zzzzzzzz ");
 	}
 
 }
