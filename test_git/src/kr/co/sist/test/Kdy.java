997c19ffaff5e34c200e2726522066a70d57fc21
qdkok <qdkok@naver.com>
2017-12-13 14:28:57 +0900

    test

--- test_git/src/kr/co/sist/test/Kdy.java
+++ test_git/src/kr/co/sist/test/Kdy.java
@@ -5,6 +5,7 @@ public class Kdy {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test");
+		System.out.println("³»²¨³¯¸®¸é ´ÙÁ×¾î");
 	}
 
 }
